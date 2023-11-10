9e712bb88cdf0dae8fd66e30d16b3ac1f0640a13
Brian Snider <brian.ms@me.com>
2023-11-10 16:20:17 -0700

    configstuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { ThumbnailComponent } from './components/thumbnail/thumbnail.component';
+import { ChairRentalComponent } from './pages/chair-rental/chair-rental.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { StylistsComponent } from './pages/stylists/stylists.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { ChairRentalComponent } from './pages/chair-rental/chair-rental.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ThumbnailComponent } from './components/thumbnail/thumbnail.component';
-import { ErrorPageComponent } from './pages/error-page/error-page.component';
-import { MainNavComponent } from './components/main-nav/main-nav.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 @NgModule({
   declarations: [
