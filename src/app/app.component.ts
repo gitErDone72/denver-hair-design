e4aa2f6e4f8f777b9874ee5351937f85ae3d0cef
Brian Snider <brian.ms@me.com>
2024-09-30 08:34:04 -0600

    services work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,8 +47,29 @@ export class AppComponent implements OnInit {
         this.isHomePage = event.url === '/';
       });
     this.isWebWidthAndBeyond();
+    this.setHomePageBackgroundImage();
   }
 
+  /**
+   * Sets the background image of the home page to a random image.
+   *
+   * This function generates a random number between 1 and 5, inclusive,
+   * and sets the CSS variable `--app-background-image` to the URL of
+   * an image corresponding to that random number. The images are
+   * expected to be located in the `/assets/img/pages/` directory and
+   * named in the format `home_<randomNumber>.jpg`.
+   */
+  setHomePageBackgroundImage(): void {
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    const root = document.documentElement;
+    root.style.setProperty('--app-background-image', `url('/assets/img/pages/home_${randomNumber}.jpg')`);
+  }
+
+  /**
+   * Retrieves the animation page name from the current route context.
+   *
+   * @returns {string} The animation page name in camel case if available, otherwise '/'.
+   */
   getAnimationPageName(): string {
     this.routeContext = this.contexts.getContext('primary');
     return this.routeContext !== null
@@ -58,6 +79,11 @@ export class AppComponent implements OnInit {
       : '/';
   }
 
+  /**
+   * Toggles the state of the mobile navigation.
+   *
+   * @param $event - A boolean value indicating whether the mobile navigation should be open or closed.
+   */
   togglePageForMobileNavigation($event: boolean) {
     this.isMoblieNavOpen = $event;
   }
