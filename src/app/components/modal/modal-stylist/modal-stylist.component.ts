69a2bd733e492dffe61652edf9b737b0763ed8e9
Brian Snider <brian.ms@me.com>
2024-11-14 00:28:04 -0700

    modal stylist tweaks

--- src/app/components/modal/modal-stylist/modal-stylist.component.ts
+++ src/app/components/modal/modal-stylist/modal-stylist.component.ts
@@ -4,13 +4,14 @@ import { BookingMethodType, IStylist } from 'src/app/shared/stylistItem.model';
 import { ThumbnailComponent } from '../../thumbnail/thumbnail.component';
 import { IThumbnailItem } from '../../thumbnail/thumbnail.model';
 import { BookingButtonComponent } from '../../booking-button/booking-button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'dhd-modal-stylist',
   standalone: true,
   templateUrl: './modal-stylist.component.html',
   host: { class: 'modal-stylist' },
-  imports: [ThumbnailComponent, BookingButtonComponent],
+  imports: [ThumbnailComponent, BookingButtonComponent, CommonModule],
 })
 export class ModalStylistComponent implements AfterContentInit {
   bookingMethodType = BookingMethodType;
